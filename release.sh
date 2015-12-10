16d8191f095c65cad0663cf25d47e42ab67610c7
Brandon Black <brandonmblack@gmail.com>
2015-12-10 02:59:52 -0800

    [minor] adding release script

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+RELEASE=$(printf "from twitter_ads.utils import get_version\nprint(get_version())" | python)
+
+# tag the release
+git tag "v$RELEASE"
+git push --tags
+
+# push to pypi
+python setup.py sdist upload
+
+# clean and build new docs
+cd docs && make clean && make html && cd ..
+
+# release new docs
+git checkout gh-pages
+rm -rf reference/*
+cp -R docs/build/html/* reference/
+git add reference
+git commit -m "\"[update] docs refresh for $RELEASE\""
+git push origin HEAD:gh-pages
+git checkout master
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# Copyright (C) 2015 Twitter, Inc.
+
 import os
 import sys
 from setuptools import setup, find_packages
