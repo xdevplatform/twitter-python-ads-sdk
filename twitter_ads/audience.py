e9984ab8cbcc0da79812228da45f5e3c1f944c1b
Brandon Black <brandonmblack@gmail.com>
2016-01-25 22:22:57 -0800

    [feature] adding support for creating and updating tailored audiences

--- twitter_ads/audience.py
+++ twitter_ads/audience.py
@@ -4,6 +4,9 @@
 
 from twitter_ads.enum import TA_OPERATIONS, TRANSFORM
 from twitter_ads.resource import resource_property, Resource
+from twitter_ads.http import TONUpload, Request
+from twitter_ads.error import BadRequest
+from twitter_ads.cursor import Cursor
 
 
 class TailoredAudience(Resource):
@@ -17,30 +20,75 @@ class TailoredAudience(Resource):
 
     @classmethod
     def create(klass, account, file_path, name, list_type):
-        raise NotImplementedError
+        """
+        Uploads and creates a new tailored audience.
+        """
+        upload = TONUpload(account.client, file_path)
+        audience = klass(account)
+        getattr(audience, '__create_audience__')(name, list_type)
+        try:
+            getattr(audience, '__update_audience__')(upload.perform(), list_type, TA_OPERATIONS.ADD)
+            return audience.reload()
+        except BadRequest as e:
+            audience.delete()
+            raise e
 
     @classmethod
     def opt_out(klass, account, file_path, list_type):
-        raise NotImplementedError
+        """
+        Updates the global opt-out list for the specified advertiser account.
+        """
+        upload = TONUpload(account.client, file_path)
+        params = {'input_file_path': upload.perform(), 'list_type': list_type}
+        resource = klass.OPT_OUT.format(account_id=account.id)
+        Request(account.client, 'put', resource, params=params).perform()
+        return True
 
     def update(self, file_path, list_type, operation=TA_OPERATIONS.ADD):
         """
         Updates the current tailored audience instance.
         """
-        raise NotImplementedError
+        upload = TONUpload(self.account.client, file_path)
+        getattr(self, '__update_audience__')(upload.perform(), list_type, operation)
+        return self.reload()
 
     def delete(self):
         """
         Deletes the current tailored audience instance.
         """
-        raise NotImplementedError
+        resource = self.RESOURCE.format(account_id=self.account.id, id=self.id)
+        response = Request(self.account.client, 'delete', resource).perform()
+        return self.from_response(response.body['data'])
 
     def status(self):
         """
-        Returns the status of all changes for the current tailored
-        audience instance.
+        Returns the status of all changes for the current tailored audience instance.
         """
-        raise NotImplementedError
+        if not self.id:
+            return None
+
+        resource = self.RESOURCE_UPDATE.format(account_id=self.account.id)
+        request = Request(self.account.client, 'get', resource, params=self.to_params())
+        cursor = list(Cursor(None, request))
+
+        return filter(lambda change: change['tailored_audience_id'] == self.id, cursor)
+
+    def __create_audience__(self, name, list_type):
+        params = {'name': name, 'list_type': list_type}
+        resource = self.RESOURCE_COLLECTION.format(account_id=self.account.id)
+        response = Request(self.account.client, 'post', resource, params=params).perform()
+        return self.from_response(response.body['data'])
+
+    def __update_audience__(self, location, list_type, operation):
+        params = {
+            'tailored_audience_id': self.id,
+            'input_file_path': location,
+            'list_type': list_type,
+            'operation': operation
+        }
+
+        resource = self.RESOURCE_UPDATE.format(account_id=self.account.id)
+        return Request(self.account.client, 'post', resource, params=params).perform()
 
 # tailored audience properties
 # read-only
