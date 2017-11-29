46e0d561af36906e9fbc7cfcabefffc5952822b1
Juan Shishido <juanshishido@cal.berkeley.edu>
2017-11-28 17:08:47 -0800

    paused to entity_status read attribute for promoted_tweets

--- twitter_ads/creative.py
+++ twitter_ads/creative.py
@@ -61,8 +61,8 @@ class PromotedTweet(Resource, Persistence, Analytics):
 resource_property(PromotedTweet, 'approval_status', readonly=True)
 resource_property(PromotedTweet, 'created_at', readonly=True, transform=TRANSFORM.TIME)
 resource_property(PromotedTweet, 'deleted', readonly=True, transform=TRANSFORM.BOOL)
+resource_property(PromotedTweet, 'entity_status', readonly=True)
 resource_property(PromotedTweet, 'id', readonly=True)
-resource_property(PromotedTweet, 'paused', readonly=True, transform=TRANSFORM.BOOL)
 resource_property(PromotedTweet, 'updated_at', readonly=True, transform=TRANSFORM.TIME)
 # writable
 resource_property(PromotedTweet, 'line_item_id')
