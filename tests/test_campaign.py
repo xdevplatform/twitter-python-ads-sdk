2891d86f70679b319c95354979c8dfa9960fc14c
Juan Shishido <juanshishido@cal.berkeley.edu>
2017-11-28 23:19:23 -0800

    update campaigns and line items tests; styling

--- tests/test_campaign.py
+++ tests/test_campaign.py
@@ -12,94 +12,56 @@ from twitter_ads import API_VERSION
 
 @responses.activate
 def test_campaigns_all():
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph'),
-            body=with_fixture('accounts_load'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph'),
+                  body=with_fixture('accounts_load'),
+                  content_type='application/json')
 
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph/campaigns'),
-            body=with_fixture('campaigns_all'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph/campaigns'),
+                  body=with_fixture('campaigns_all'),
+                  content_type='application/json')
 
     client = Client(
-            characters(40),
-            characters(40),
-            characters(40),
-            characters(40)
-            )
+        characters(40),
+        characters(40),
+        characters(40),
+        characters(40)
+    )
 
     account = Account.load(client, '2iqph')
 
     cursor = account.campaigns()
-
     assert cursor is not None
     assert isinstance(cursor, Cursor)
     assert cursor.count == 10
 
+    campaign = cursor.next()
+    assert campaign.id == '2wap7'
+    assert campaign.entity_status == 'ACTIVE'
+
 
 @responses.activate
 def test_campaign_load():
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph'),
-            body=with_fixture('accounts_load'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph'),
+                  body=with_fixture('accounts_load'),
+                  content_type='application/json')
 
-    responses.add(
-            responses.GET,
-            with_resource(
-                '/' + API_VERSION + '/accounts/2iqph/campaigns/2wap7'),
-            body=with_fixture('campaigns_load'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph/campaigns/2wap7'),
+                  body=with_fixture('campaigns_load'),
+                  content_type='application/json')
 
     client = Client(
-            characters(40),
-            characters(40),
-            characters(40),
-            characters(40)
-            )
+        characters(40),
+        characters(40),
+        characters(40),
+        characters(40)
+    )
 
     account = Account.load(client, '2iqph')
 
     campaign = Campaign.load(account, '2wap7')
-
-    assert campaign
-
-
-@responses.activate
-def test_campaign_entity_status_exists():
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph'),
-            body=with_fixture('accounts_load'),
-            content_type='application/json',
-            )
-
-    responses.add(
-            responses.GET,
-            with_resource(
-                '/' + API_VERSION + '/accounts/2iqph/campaigns/2wap7'),
-            body=with_fixture('campaigns_load'),
-            content_type='application/json',
-            )
-
-    client = Client(
-            characters(40),
-            characters(40),
-            characters(40),
-            characters(40)
-            )
-
-    account = Account.load(client, '2iqph')
-
-    campaign = Campaign.load(account, '2wap7')
-
-    assert campaign.entity_status
+    assert campaign.id == '2wap7'
     assert campaign.entity_status == 'ACTIVE'
--- tests/test_line_item.py
+++ tests/test_line_item.py
@@ -12,94 +12,56 @@ from twitter_ads import API_VERSION
 
 @responses.activate
 def test_line_items_all():
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph'),
-            body=with_fixture('accounts_load'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph'),
+                  body=with_fixture('accounts_load'),
+                  content_type='application/json')
 
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph/line_items'),
-            body=with_fixture('line_items_all'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph/line_items'),
+                  body=with_fixture('line_items_all'),
+                  content_type='application/json')
 
     client = Client(
-            characters(40),
-            characters(40),
-            characters(40),
-            characters(40)
-            )
+        characters(40),
+        characters(40),
+        characters(40),
+        characters(40)
+    )
 
     account = Account.load(client, '2iqph')
 
     cursor = account.line_items()
-
     assert cursor is not None
     assert isinstance(cursor, Cursor)
     assert cursor.count == 10
 
+    lineitem = cursor.next()
+    assert lineitem.id == 'bw2'
+    assert lineitem.entity_status == 'ACTIVE'
+
 
 @responses.activate
 def test_line_item_load():
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph'),
-            body=with_fixture('accounts_load'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph'),
+                  body=with_fixture('accounts_load'),
+                  content_type='application/json')
 
-    responses.add(
-            responses.GET,
-            with_resource(
-                '/' + API_VERSION + '/accounts/2iqph/line_items/bw2'),
-            body=with_fixture('line_items_load'),
-            content_type='application/json',
-            )
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph/line_items/bw2'),
+                  body=with_fixture('line_items_load'),
+                  content_type='application/json')
 
     client = Client(
-            characters(40),
-            characters(40),
-            characters(40),
-            characters(40)
-            )
+        characters(40),
+        characters(40),
+        characters(40),
+        characters(40)
+    )
 
     account = Account.load(client, '2iqph')
 
     line_item = LineItem.load(account, 'bw2')
-
-    assert line_item
-
-
-@responses.activate
-def test_line_item_entity_status_exists():
-    responses.add(
-            responses.GET,
-            with_resource('/' + API_VERSION + '/accounts/2iqph'),
-            body=with_fixture('accounts_load'),
-            content_type='application/json',
-            )
-
-    responses.add(
-            responses.GET,
-            with_resource(
-                '/' + API_VERSION + '/accounts/2iqph/line_items/bw2'),
-            body=with_fixture('line_items_load'),
-            content_type='application/json',
-            )
-
-    client = Client(
-            characters(40),
-            characters(40),
-            characters(40),
-            characters(40)
-            )
-
-    account = Account.load(client, '2iqph')
-
-    line_item = LineItem.load(account, 'bw2')
-
-    assert line_item.entity_status
+    assert line_item.id == 'bw2'
     assert line_item.entity_status == 'ACTIVE'
