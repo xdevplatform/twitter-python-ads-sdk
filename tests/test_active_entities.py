91a86668e4bf86b8d7ac9b5c132f99edecaac7a5
smaeda-ks <irt_m.jrsyo@ntworkers.com>
2019-07-22 01:35:29 +0900

    Fix Active Entites test
    - fix function name
    - add new assert to check request URL (query parameter) to make sure optional parameters are passed as intended

--- tests/test_active_entities.py
+++ tests/test_active_entities.py
@@ -7,12 +7,11 @@ from datetime import datetime, timedelta
 from twitter_ads.account import Account
 from twitter_ads.client import Client
 from twitter_ads.campaign import Campaign
-from twitter_ads.resource import Analytics
 from twitter_ads import API_VERSION
 
 
 @responses.activate
-def test_tweet_previews_load():
+def test_active_entities():
     responses.add(responses.GET,
                   with_resource('/' + API_VERSION + '/accounts/2iqph'),
                   body=with_fixture('accounts_load'),
@@ -35,9 +34,16 @@ def test_tweet_previews_load():
     end_time = datetime.utcnow().date()
     start_time = end_time - timedelta(days=1)
 
-    active_entities = Campaign.active_entities(account, start_time, end_time)
+    active_entities = Campaign.active_entities(
+        account,
+        start_time,
+        end_time,
+        campaign_ids=['foo', 'bar']
+    )
 
+    assert len(responses.calls) == 2
+    assert 'campaign_ids=foo%2Cbar' in responses.calls[1].request.url
     assert active_entities is not None
-    assert type(active_entities) is list
+    assert isinstance(active_entities, list)
     assert len(active_entities) == 4
     assert active_entities[0]['entity_id'] == '2mvb28'
