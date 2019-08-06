105082ca3d8e2328741e0b7dda236512a3061556
smaeda-ks <irt_m.jrsyo@ntworkers.com>
2019-08-06 15:47:44 +0900

    Add test for decorator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,30 @@
 import datetime
-from random import randint
 
 from twitter_ads.enum import GRANULARITY
-from twitter_ads.utils import to_time
+from twitter_ads.utils import to_time, Deprecated
 
 
 t = datetime.datetime(2006, 3, 21, 0, 0, 0)
+
 
 def test_to_time_based_on_granularity():
     for g in [None, GRANULARITY.HOUR, GRANULARITY.TOTAL]:
         assert to_time(t, g) == '2006-03-21T00:00:00Z'
     assert to_time(t, GRANULARITY.DAY) == '2006-03-21'
+
+
+def test_decorator_deprecated():
+    import warnings
+
+    class TestClass(object):
+        @classmethod
+        @Deprecated('deprecated API')
+        def test(self):
+            pass
+
+    with warnings.catch_warnings(record=True) as log:
+        TestClass.test()
+        assert len(log) == 1
+        assert issubclass(log[-1].category, DeprecationWarning)
+        assert "TestClass.test" in str(log[-1].message)
+        assert "deprecated API" in str(log[-1].message)
