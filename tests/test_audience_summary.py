566ff0a32f3609556f089015563fb14a918bb75d
Tushar Bhushan <tbhushan@twitter.com>
2020-03-10 13:48:14 -0700

    added audience summary and tests

---
+++ tests/test_audience_summary.py
@@ -0,0 +1,62 @@
+import responses
+import unittest
+
+from tests.support import with_resource, with_fixture, characters
+
+from twitter_ads.account import Account
+from twitter_ads.client import Client
+from twitter_ads.targeting import AudienceSummary
+from twitter_ads import API_VERSION
+
+
+@responses.activate
+def test_audience_summary():
+    responses.add(responses.GET,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph'),
+                  body=with_fixture('accounts_load'),
+                  content_type='application/json')
+
+    responses.add(responses.POST,
+                  with_resource('/' + API_VERSION + '/accounts/2iqph/audience_summary'),
+                  body=with_fixture('audience_summary'),
+                  content_type='application/json')
+
+    client = Client(
+        characters(40),
+        characters(40),
+        characters(40),
+        characters(40)
+    )
+
+    account = Account.load(client, '2iqph')
+
+    params = {
+        "targeting_criteria": [
+          {
+            "targeting_type":"LOCATION",
+            "targeting_value":"96683cc9126741d1"
+          },
+          {
+            "targeting_type":"BROAD_KEYWORD",
+            "targeting_value":"cats"
+          },
+          {
+            "targeting_type":"SIMILAR_TO_FOLLOWERS_OF_USER",
+            "targeting_value": "14230524"
+          },
+          {
+            "targeting_type":"SIMILAR_TO_FOLLOWERS_OF_USER",
+            "targeting_value": "90420314"
+          }
+        ]
+      }
+
+    audience_summary = AudienceSummary.load(
+        account=account,
+        params=params
+    )
+
+    assert audience_summary is not None
+    assert audience_summary['audience_size'] is not None
+    assert audience_summary['audience_size']['min'] == 41133600
+    assert audience_summary['audience_size']['max'] == 50274400
--- twitter_ads/targeting.py
+++ twitter_ads/targeting.py
@@ -3,7 +3,10 @@
 """Container for all targeting related logic used by the Ads API SDK."""
 
 from twitter_ads.http import Request
+from twitter_ads.resource import resource_property, Resource, Persistence
 from twitter_ads import API_VERSION
+from twitter_ads.utils import FlattenParams
+import json
 
 
 class AudienceSummary(Resource, Persistence):
@@ -11,12 +14,15 @@ class AudienceSummary(Resource, Persistence):
 
 	RESOURCE = '/' + API_VERSION + '/accounts/{account_id}/audience_summary'
 
-	def load(self, params):
-		resource = self.RESOURCE.format(account_id=account.id)
+	@classmethod
+	@FlattenParams
+	def load(klass, account, params):
+		resource = klass.RESOURCE.format(account_id=account.id)
 		headers = {'Content-Type': 'application/json'}
-        response = Request(self.account.client,
-                           'post',
-                           resource,
-                           headers=headers,
-                           body=json.dumps(params)).perform()
-        return self.from_response(response.body['data'])
+		response = Request(account.client,
+							'post',
+							resource,
+							headers=headers,
+							body=json.dumps(params)).perform()
+		return response.body['data']
+
