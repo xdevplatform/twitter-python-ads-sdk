2c9c716ce33f4abc8bf42047be13968436b77678
BJapac <70250690+BJapac@users.noreply.github.com>
2021-04-26 11:51:56 +0900

    Create custom_audience.py

---
+++ examples/custom_audience.py
@@ -0,0 +1,39 @@
+import hashlib
+from twitter_ads.client import Client
+from twitter_ads.audience import CustomAudience
+
+CONSUMER_KEY = 'your consumer key'
+CONSUMER_SECRET = 'your consumer secret'
+ACCESS_TOKEN = 'access token'
+ACCESS_TOKEN_SECRET = 'access token secret'
+ACCOUNT_ID = 'account id'
+
+# initialize the client
+client = Client(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+# load the advertiser account instance
+account = client.accounts(ACCOUNT_ID)
+
+# create a new custom audience
+audience = CustomAudience.create(account, 'test TA')
+
+# sample user
+# all values musth be sha256 hashed
+email_hash = hashlib.sha256("test-email@test.com").hexdigest()
+
+# create payload
+user = [{
+    "operation_type": "Update",
+    "params": {
+        "users": [{
+            "email": [
+                email_hash
+            ]
+        }]
+    }
+}]
+
+# update the custom audience
+success_count, total_count = audience.users(user)
+if success_count == total_count:
+    print(("Successfully added {total_count} users").format(total_count=total_count))
