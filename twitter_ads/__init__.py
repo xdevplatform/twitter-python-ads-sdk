60cea44d570248f8de86c14304be3adb90c17f66
Tom Osowski <tosowski@twitter.com>
2021-09-02 16:08:10 -0700

    Revert "Remove deprecated cards endpoints (#276)" (#281)
    This reverts commit 462d3ca5f1c4d28e23c3a841810b5c09ee5e6f40.

--- twitter_ads/__init__.py
+++ twitter_ads/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2015 Twitter, Inc.
 
-VERSION = (9, 0, 0)
+VERSION = (9, 0, 1)
 API_VERSION = '9'
 
 from twitter_ads.utils import get_version
--- twitter_ads/account.py
+++ twitter_ads/account.py
@@ -10,7 +10,7 @@ from twitter_ads import API_VERSION
 
 from twitter_ads.resource import resource_property, Resource
 from twitter_ads.creative import (AccountMedia, MediaCreative, ScheduledTweet,
-                                  Card, PromotedTweet)
+                                  Card, VideoWebsiteCard, PromotedTweet)
 from twitter_ads.audience import CustomAudience
 from twitter_ads.campaign import (AppList, Campaign, FundingInstrument, LineItem,
                                   PromotableUser, ScheduledPromotedTweet)
@@ -147,6 +147,12 @@ class Account(Resource):
         Returns a collection of Scheduled Promoted Tweets available to the current account.
         """
         return self._load_resource(ScheduledPromotedTweet, id, **kwargs)
+
+    def video_website_cards(self, id=None, **kwargs):
+        """
+        Returns a collection of video website cards available to the current account.
+        """
+        return self._load_resource(VideoWebsiteCard, id, **kwargs)
 
     def cards(self, id=None, **kwargs):
         """
